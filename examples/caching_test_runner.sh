af362463a37daf1e84797e1fc5fa92c358d4d508
Guillaume Maudoux <guillaume.maudoux@tweag.io>
2025-11-17 15:46:28 +0100

    Try to be more aggressive

--- examples/caching_test_runner.sh
+++ examples/caching_test_runner.sh
@@ -13,7 +13,7 @@ exit_with_msg() {
 
 # Hardcode for now
 bazel_cmds=(
-  "build //... --nohome_rc --announce_rc --config=fawltydeps --keep_going --verbose_failures"
+  "build //... --nosystem_rc --nohome_rc --announce_rc --config=fawltydeps --keep_going --verbose_failures"
 )
 args=()
 
